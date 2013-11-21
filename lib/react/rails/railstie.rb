f299db0f5734321007b908a588b163de0dbc953f
Paul O’Shannessy <paul@oshannessy.com>
2013-11-20 18:47:21 -0800

    Add addon config
    This makes it possible to use the addons build of React.
    
    Closes #13

--- lib/react/rails/railstie.rb
+++ lib/react/rails/railstie.rb
@@ -14,11 +14,13 @@ module React
         # common mistakes. These are all stripped out in the minified build.
         if variant = app.config.react.variant || ::Rails.env.test?
           variant ||= :development
+          addons = app.config.react.addons || false
+
           # Copy over the variant into a path that sprockets will pick up.
           # We'll always copy to 'react.js' so that no includes need to change.
           # We'll also always copy of JSXTransformer.js
           tmp_path = app.root.join('tmp/react-rails')
-          filename = 'react' + (variant == :production ? '.min.js' : '.js')
+          filename = 'react' + (addons ? '-with-addons' : '') + (variant == :production ? '.min.js' : '.js')
           FileUtils.mkdir_p(tmp_path)
           FileUtils.cp(::React::Source.bundled_path_for(filename),
                        tmp_path.join('react.js'))
