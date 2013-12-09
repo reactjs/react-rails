14b4df73fde0aa2c772871b2f7de33f90b480c7a
Jakub Malinowski <jakubmal@gmail.com>
2013-12-09 22:50:47 +0100

    Bump up version to stop being dependent on react versioning

--- lib/react/rails/version.rb
+++ lib/react/rails/version.rb
@@ -2,7 +2,6 @@ module React
   module Rails
     # Version numbers will track react-source, but we'll add another level so
     # that we can increment, but have some amount of stability.
-    VERSION = '0.8.0.0'
+    VERSION = '1.0.0.beta1'
   end
 end
-
