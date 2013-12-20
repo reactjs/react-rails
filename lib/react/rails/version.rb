99638c2ea24893dbc7d542518975362a78b903d7
Paul O’Shannessy <paul@oshannessy.com>
2013-12-19 18:42:41 -0800

    Update to react-source 0.5.2

--- lib/react/rails/version.rb
+++ lib/react/rails/version.rb
@@ -2,7 +2,7 @@ module React
   module Rails
     # Version numbers will track react-source, but we'll add another level so
     # that we can increment, but have some amount of stability.
-    VERSION = '0.5.1.0'
+    VERSION = '0.5.2.0'
   end
 end
 
