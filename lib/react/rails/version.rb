e389a9618a35d633924faa8a820b1acf5032ea68
Paul O’Shannessy <paul@oshannessy.com>
2014-02-16 12:55:25 -0800

    Update version, not a beta yet

--- lib/react/rails/version.rb
+++ lib/react/rails/version.rb
@@ -1,7 +1,5 @@
 module React
   module Rails
-    # Version numbers will track react-source, but we'll add another level so
-    # that we can increment, but have some amount of stability.
-    VERSION = '1.0.0.beta1'
+    VERSION = '1.0.0-pre'
   end
 end
