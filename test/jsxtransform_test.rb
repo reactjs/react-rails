261a2c9c81c339f7c9ae5e7fcb90f9b49a0b8258
Jakub Malinowski <jakubmal@gmail.com>
2014-04-09 01:40:31 +0200

    Fix failing Travis tests due to coffee-script version mismatch

--- test/jsxtransform_test.rb
+++ test/jsxtransform_test.rb
@@ -34,7 +34,14 @@ class JSXTransformTest < ActionDispatch::IntegrationTest
   test 'asset pipeline should transform JSX + Coffeescript' do
     get 'assets/example2.js'
     assert_response :success
-    assert_equal EXPECTED_JS_2, @response.body
+
+    # Different coffee-script may generate slightly different outputs:
+    #  1. Some version inserts an extra "\n" at the beginning.
+    #  2. "/** @jsx React.DOM */" and "/** @jsx React.DOM*/" are both possible.
+    #
+    # Because appraisal is used, multiple versions of coffee-script are treated
+    # together. Remove all spaces to make test pass.
+    assert_equal EXPECTED_JS_2.gsub(/\s/, ''), @response.body.gsub(/\s/, '')
   end
 
 end
