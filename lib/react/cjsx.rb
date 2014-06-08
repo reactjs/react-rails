d8b01c077508ae0465cdd04fee61c6369e068585
Robert Mosolgo <rdmosolgo@gmail.com>
2014-06-07 17:59:29 -0700

    use static file instead of cjsx executable

--- lib/react/cjsx.rb
+++ lib/react/cjsx.rb
@@ -4,8 +4,18 @@ require 'react/cjsx/template'
 
 module React
   module CJSX
+    def self.context
+      cjsx_path = File.expand_path('../../../vendor/assets/javascripts/CJSXTransformer.js', __FILE__)
+      contents =
+        # If execjs uses therubyracer, there is no 'global'. Make sure
+        # we have it so CJSX script can work properly.
+        'var global = global || this;' +
+        File.read(cjsx_path)
+      @context ||= ExecJS.compile(contents)
+    end
+
     def self.transform(code)
-      result = `echo "#{code}" | cjsx -sc`
+      result = context.call('CJSXTransformer.transform', code)
       return result
     end
   end
