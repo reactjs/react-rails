75a3188495a1602558d13b6c751cf3d194a48f73
Alec Hipshear <alechipshear@gmail.com>
2014-07-13 11:32:01 -0400

    Kludge to fix Tubrolinks intermittent test errors
    Sometimes Turbolinks hasn't fully loaded before Capybara attempts to
    call 'Turbolinks.visit'. Adding a brief sleep gives Turbolinks a chance
    to become availabe to PhantomJS

--- test/view_helper_test.rb
+++ test/view_helper_test.rb
@@ -66,6 +66,10 @@ class ViewHelperTest < ActionDispatch::IntegrationTest
     page.execute_script('Turbolinks.visit("/pages/2");')
     assert page.has_content?('Hello Alice')
 
+    # Sometimes Turbolinks isn't available right away. We need to wait for a brief moment
+    # for phantomJS to finish loading javascript.
+    sleep(0.1)
+
     page.execute_script('Turbolinks.visit("/pages/1");')
     assert page.has_content?('Hello Bob')
 
