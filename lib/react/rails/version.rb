a50fc7072d91f1640b5b15c3562ea171badd0591
Paul O’Shannessy <paul@oshannessy.com>
2014-11-06 15:25:47 -0800

    Bump version for 0.12.0.0

--- lib/react/rails/version.rb
+++ lib/react/rails/version.rb
@@ -2,7 +2,7 @@ module React
   module Rails
     # Version numbers will track react-source, but we'll add another level so
     # that we can increment, but have some amount of stability.
-    VERSION = '0.11.1.0'
+    VERSION = '0.12.0.0'
   end
 end
 
