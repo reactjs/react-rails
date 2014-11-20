30f5a7dff62691782dc007e994fe9df507cdc964
Gabe Scholz <gabe@brewhouse.io>
2014-11-19 19:28:21 -0800

    Create render_react_component controller helper

---
+++ lib/react/rails/controller_helper.rb
@@ -0,0 +1,20 @@
+module React
+  module Rails
+    module ControllerHelper
+
+      # Render a React component as the view instead of
+      # a template from the app/views folder.
+      #
+      def render_react_component(*args, &block)
+        current_layout = _layout.virtual_path
+        @__react_component_args = args
+        @__react_component_block = block
+
+        render inline: '<%= react_component(*@__react_component_args, &@__react_component_block) %>', layout: current_layout
+      end
+
+    end
+  end
+end
+
+ActionController::Base.send(:include, React::Rails::ControllerHelper) if defined?(Rails)
