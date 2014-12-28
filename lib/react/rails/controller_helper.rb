fefaa818b2fcb80dab5c4c0f17edc76eeaab8f84
Gabe Scholz <gabe@brewhouse.io>
2014-12-27 18:22:02 -0800

    Allow #render_react_component to pass in layout and status options

--- lib/react/rails/controller_helper.rb
+++ lib/react/rails/controller_helper.rb
@@ -5,12 +5,20 @@ module React
       # Render a React component as the view instead of
       # a template from the app/views folder.
       #
-      def render_react_component(*args, &block)
-        current_layout = _layout.virtual_path
-        @__react_component_args = args
+      def render_react_component(name, args = {}, options = {}, &block)
+        __react_render_component_args = {
+          inline: '<%= react_component(*@__react_component_args, &@__react_component_block) %>',
+          layout: options.delete(:layout) || _layout.virtual_path
+        }
+
+        if status = options.delete(:status)
+          __react_render_component_args.merge!(status: status)
+        end
+
+        @__react_component_args = [name, args, options]
         @__react_component_block = block
 
-        render inline: '<%= react_component(*@__react_component_args, &@__react_component_block) %>', layout: current_layout
+        render __react_render_component_args
       end
 
     end
