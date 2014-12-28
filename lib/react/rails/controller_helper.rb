260bce65cb12c25c843d2556034f4f6d42bb0ee3
Gabe Scholz <gabe@brewhouse.io>
2014-12-27 18:57:06 -0800

    Fix variable name

--- lib/react/rails/controller_helper.rb
+++ lib/react/rails/controller_helper.rb
@@ -6,19 +6,19 @@ module React
       # a template from the app/views folder.
       #
       def render_react_component(name, args = {}, options = {}, &block)
-        __react_render_component_args = {
+        __render_react_component_args = {
           inline: '<%= react_component(*@__react_component_args, &@__react_component_block) %>',
           layout: options.delete(:layout) || _layout.virtual_path
         }
 
         if status = options.delete(:status)
-          __react_render_component_args.merge!(status: status)
+          __render_react_component_args.merge!(status: status)
         end
 
         @__react_component_args = [name, args, options]
         @__react_component_block = block
 
-        render __react_render_component_args
+        render __render_react_component_args
       end
 
     end
