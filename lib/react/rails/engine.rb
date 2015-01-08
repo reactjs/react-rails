addda86e415eaaf6c841c1e4ed3f37462accfa78
Dan Ott <danott@users.noreply.github.com>
2015-01-07 21:38:56 -0800

    Use isolate_namespace in Engine.

--- lib/react/rails/engine.rb
+++ lib/react/rails/engine.rb
@@ -1,6 +1,8 @@
 module React
   module Rails
     class Engine < ::Rails::Engine
+      isolate_namespace React
+
       initializer "react_rails.setup_engine", :group => :all do |app|
         app.assets.register_engine '.jsx', React::JSX::Template
       end
