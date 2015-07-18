f88c3e9598ef5f44bb91f33fcc67096ef8c1a5d9
Vipul A M <vipulnsward@gmail.com>
2015-07-18 15:01:47 +0530

    - Use Sprockets.register_engine if we are on latest master

--- lib/react/rails/engine.rb
+++ lib/react/rails/engine.rb
@@ -3,7 +3,7 @@ module React
     class Engine < ::Rails::Engine
       initializer "react_rails.setup_engine", :group => :all do |app|
         if app.assets.nil?
-         app.config.assets.register_engine '.jsx', React::JSX::Template
+          Sprockets.register_engine  '.jsx', React::JSX::Template
         else
           app.assets.register_engine '.jsx', React::JSX::Template
         end
