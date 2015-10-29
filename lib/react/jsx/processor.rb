a42be1e38abf24a704fa103c6f90862330dc0d05
Eugenijus Radlinskas <eugenijus.radlinskas@gmail.com>
2015-10-29 15:31:45 +0200

    Simplify JSX processor

--- lib/react/jsx/processor.rb
+++ lib/react/jsx/processor.rb
@@ -1,16 +1,8 @@
 module React
   module JSX
     class Processor
-      def self.instance
-        @instance ||= new
-      end
-      
       def self.call(input)
-        instance.call(input)
-      end
-
-      def call(input)
-        {data: JSX::transform(input[:data])}
+        JSX::transform(input[:data])
       end
     end
   end
