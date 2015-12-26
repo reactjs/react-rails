905f61a1269e0580f882011b9b1ed91718a27ec1
Robert Mosolgo <rdmosolgo@gmail.com>
2015-12-26 11:12:55 -0500

    Fix(SprocketsRenderer) support Sprockets::Manifest if assets were precompiled

--- lib/react/server_rendering/sprockets_renderer.rb
+++ lib/react/server_rendering/sprockets_renderer.rb
@@ -15,7 +15,7 @@ module React
         js_code = CONSOLE_POLYFILL.dup
 
         filenames.each do |filename|
-          js_code << ::Rails.application.assets[filename].to_s
+          js_code << get_asset_content(filename)
         end
 
         super(options.merge(code: js_code))
@@ -38,6 +38,31 @@ module React
 
       def after_render(component_name, props, prerender_options)
         @replay_console ? CONSOLE_REPLAY : ""
+      end
+
+      # Given an asset name, return the fully-compiled body of that asset.
+      #
+      # Out of the box, it supports a Sprockets::Environment (application.assets)
+      # and a Sprockets::Manifest (application.assets_manifest), which covers the
+      # default Rails setups.
+      #
+      # Make a `server.js` which has `//= require react-server` and `//= require components`.
+      # Then add `server` to the precompile list, eg `Rails.application.config.assets.precompile += %w( server.js )`.
+      #
+      # In production, react-rails will use the precompiled file.
+      #
+      # TODO: what if the assets aren't on the local server (maybe they're on a CDN?)
+      # Can we check for asset_host configuration here?
+      def get_asset_content(asset_name)
+        if ::Rails.application.config.assets.compile
+          ::Rails.application.assets[asset_name].to_s
+        else
+          manifest = ::Rails.application.assets_manifest
+          # Find the corresponding compiled file:
+          asset_path = manifest.assets[asset_name] || raise("No compiled asset for #{asset_name}, was it precompiled?")
+          asset_full_path = ::Rails.root.join("public", manifest.directory, asset_path)
+          File.read(asset_full_path)
+        end
       end
     end
   end
