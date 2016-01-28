07e0e8183d5a7c7f9b943f2ce244bc6c0d1da470
Dan Ott <thedanielott@gmail.com>
2016-01-28 08:19:29 -0800

    Use a respond_to? strategy instead

--- lib/react/rails/controller_lifecycle.rb
+++ lib/react/rails/controller_lifecycle.rb
@@ -5,10 +5,10 @@ module React
 
       included do
         # use both names to support Rails 3..5
-        before_action_with_fallback = begin method(:before_action); rescue method(:before_filter); end
-        after_action_with_fallback = begin method(:after_action); rescue method(:after_filter); end
-        before_action_with_fallback.call :setup_react_component_helper
-        after_action_with_fallback.call :teardown_react_component_helper
+        before_action_with_fallback = respond_to?(:before_action) ? :before_action : :before_filter
+        after_action_with_fallback = respond_to?(:after_action) ? :after_action : :after_filter
+        public_send(before_action_with_fallback, :setup_react_component_helper)
+        public_send(after_action_with_fallback, :teardown_react_component_helper)
         attr_reader :__react_component_helper
       end
 
