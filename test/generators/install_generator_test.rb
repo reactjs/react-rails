a92d002a0aa40e2f6c28319dd41b1ee11f241c30
Akshay <akshaymohite31@yahoo.com>
2016-02-12 13:38:12 +0530

    Fixing install generator to have inserted content properly on the new line.
    - Added new line matcher regex (\s+\n) in inject_react for inserting after turbolinks.
    - Modified tests to check new lines in the inserted content from install_generator.

--- test/generators/install_generator_test.rb
+++ test/generators/install_generator_test.rb
@@ -30,15 +30,14 @@ class InstallGeneratorTest < Rails::Generators::TestCase
     File.delete destination_root + '/app/assets/javascripts/application.js'
 
     run_generator
-
-    assert_application_file_modified
+    assert_application_file_created
   end
 
   test "modifies `application.js` if it's empty" do
     init_application_js ''
 
     run_generator
-    assert_application_file_modified
+    assert_application_file_created
   end
 
   test "updates `application.js` if require_tree is commented" do
@@ -68,9 +67,14 @@ class InstallGeneratorTest < Rails::Generators::TestCase
     File.write destination_root + '/app/assets/javascripts/application.js', content
   end
 
+  def assert_application_file_created
+    assert_file 'app/assets/javascripts/application.js',
+                %r{//= require react\n//= require react_ujs\n//= require components\n}
+  end
+
   def assert_application_file_modified
-    assert_file 'app/assets/javascripts/application.js', %r{//= require react}
-    assert_file 'app/assets/javascripts/application.js', %r{//= require react_ujs}
-    assert_file 'app/assets/javascripts/application.js', %r{//= require components}
+    assert_file 'app/assets/javascripts/application.js', %r{\n//= require react\n}
+    assert_file 'app/assets/javascripts/application.js', %r{\n//= require react_ujs\n}
+    assert_file 'app/assets/javascripts/application.js', %r{\n//= require components\n}
   end
 end
