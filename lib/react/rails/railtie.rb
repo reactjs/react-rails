d70752d18dce7a03df195aebb76c4de7a55e4047
David Judd <david@academia.edu>
2016-06-29 15:26:26 -0700

    Support .js.jsx.coffee files with Sprockets 4

--- lib/react/rails/railtie.rb
+++ lib/react/rails/railtie.rb
@@ -104,6 +104,8 @@ module React
           if Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new("4.x")
             sprockets_env.register_mime_type("application/jsx", extensions: [".jsx", ".js.jsx", ".es.jsx", ".es6.jsx"])
             sprockets_env.register_transformer("application/jsx", "application/javascript", React::JSX::Processor)
+            sprockets_env.register_mime_type("application/jsx+coffee", extensions: [".jsx.coffee", ".js.jsx.coffee"])
+            sprockets_env.register_transformer("application/jsx+coffee", "application/jsx", Sprockets::CoffeeScriptProcessor)
           elsif Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new("3.0.0")
             sprockets_env.register_engine(".jsx", React::JSX::Processor, mime_type: "application/javascript")
           else
