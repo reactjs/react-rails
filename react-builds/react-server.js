54772f79cf883dfd422c874601c988b3aa6cdd82
Nick Maher <nick.maher@appearhere.co.uk>
2016-10-14 12:30:06 +0100

    Remove reference to window in react server builds
    Replacing window with global enables the removal of the window object
    shim in ExecJS render

--- react-builds/react-server.js
+++ react-builds/react-server.js
@@ -1,7 +1,5 @@
 var React = require("react");
-var ReactDOM = require("react-dom");
 var ReactDOMServer = require("react-dom/server");
 
-window.React = React;
-window.ReactDOM = ReactDOM;
-window.ReactDOMServer = ReactDOMServer;
+global.React = React;
+global.ReactDOMServer = ReactDOMServer;
