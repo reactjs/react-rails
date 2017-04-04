05f6a3c9a8c7365dd7d6ae5892d0890ea5a934ad
Robert Mosolgo <rdmosolgo@gmail.com>
2017-04-04 15:11:03 -0400

    feat(ServerRendering) add end-to-end test of per-request prerenderer

--- Appraisals
+++ Appraisals
@@ -44,12 +44,18 @@ appraise "rails-4.2-sprockets_4" do
   gem "sprockets", "~> 4.0.x"
   gem "turbolinks", "~> 2.5.0"
   gem "webpacker", github: "rails/webpacker"
+  # This ExecJS backend provides stateful context
+  # which the default nodejs backend does not
+  gem "mini_racer"
 end
 
 appraise "rails-5" do
   gem 'rails', '~> 5.0.0'
   gem "turbolinks", "~> 5.0.0"
   gem "webpacker", github: "rails/webpacker"
+  # This ExecJS backend provides stateful context
+  # which the default nodejs backend does not
+  gem "therubyracer"
 end
 
 appraise "rails-5-no_sprockets" do
--- test/react/rails/pages_controller_test.rb
+++ test/react/rails/pages_controller_test.rb
@@ -5,4 +5,20 @@ class PagesControllerTest < ActionController::TestCase
     get :show, id: 1
     assert_equal(200, response.status)
   end
+
+  when_stateful_js_context_available do
+    test "it sets up and tears down a react context" do
+      get :show, id: 1, prerender: true
+      assert_includes(response.body, "Hello")
+
+      get :show, id: 1, prerender: true, greeting: "Howdy"
+      assert_includes(response.body, "Howdy")
+
+      get :show, id: 1, prerender: true, greeting: "👋"
+      assert_includes(response.body, "👋")
+
+      get :show, id: 1, prerender: true
+      assert_includes(response.body, "Hello")
+    end
+  end
 end
