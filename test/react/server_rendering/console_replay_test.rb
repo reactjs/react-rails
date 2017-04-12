c8b7756c77d0f2c524812107dae910bba26d22d6
Robert Mosolgo <rdmosolgo@gmail.com>
2017-04-12 17:53:56 -0400

    make sure replay is true

--- test/react/server_rendering/console_replay_test.rb
+++ test/react/server_rendering/console_replay_test.rb
@@ -1,22 +1,25 @@
 require "test_helper"
 
-class ConsoleReplayTest < ActionDispatch::IntegrationTest
-  setup do
-    WebpackerHelpers.compile
-    React::ServerRendering.reset_pool
-  end
+if WebpackerHelpers.available? || SprocketsHelpers.available?
+  class ConsoleReplayTest < ActionDispatch::IntegrationTest
+    setup do
+      WebpackerHelpers.compile
+      React::ServerRendering.renderer_options = {replay_console: true}
+      React::ServerRendering.reset_pool
+    end
 
-  EXPECTED_REPLAY = <<-HTML
+    EXPECTED_REPLAY = <<-HTML
 <script class="react-rails-console-replay">
 console.log.apply(console, ["Test Console Replay"]);
 </script>
-  HTML
+    HTML
 
-  test "it clears the state between each request" do
-    # Each request should only contain one log:
-    get '/server/1'
-    assert_includes(response.body, EXPECTED_REPLAY)
-    get '/server/1'
-    assert_includes(response.body, EXPECTED_REPLAY)
+    test "it clears the state between each request" do
+      # Each request should only contain one log:
+      get '/server/1'
+      assert_includes(response.body, EXPECTED_REPLAY)
+      get '/server/1'
+      assert_includes(response.body, EXPECTED_REPLAY)
+    end
   end
 end
