7b1b5adedff5c388c4bb8134111ded8aaf8caa11
Greg <neonmd@hotmail.co.uk>
2017-09-11 14:37:44 +0100

    Tweaking configs and requires to sensible version

--- test/dummy_webpacker2/config/webpack/loaders/babel.js
+++ test/dummy_webpacker2/config/webpack/loaders/babel.js
@@ -1,5 +1,10 @@
 module.exports = {
   test: /\.js(\.erb)?$/,
   exclude: /node_modules/,
-  loader: 'babel-loader'
+  loader: 'babel-loader',
+  options: {
+    presets: [
+      ['env', { modules: false }]
+    ]
+  }
 }
