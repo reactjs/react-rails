3980169ef0804325c75c680ec5bcaa0c6213c6e1
Greg <neonmd@hotmail.co.uk>
2017-09-20 10:38:58 +0100

    Turns out Rails 4 is unhappy with params hash

--- test/react/rails/pages_controller_test.rb
+++ test/react/rails/pages_controller_test.rb
@@ -13,16 +13,16 @@ class PagesControllerTest < ActionController::TestCase
 
   when_stateful_js_context_available do
     test 'it sets up and tears down a react context' do
-      get :show, params: { id: 1, prerender: true }
+      get :show, id: 1, prerender: true
       assert_includes(response.body, 'Hello')
 
-      get :show, params: { id: 1, prerender: true, greeting: 'Howdy' }
+      get :show, id: 1, prerender: true, greeting: 'Howdy'
       assert_includes(response.body, 'Howdy')
 
-      get :show, params: { id: 1, prerender: true, greeting: '👋' }
+      get :show, id: 1, prerender: true, greeting: '👋'
       assert_includes(response.body, '👋')
 
-      get :show, params: { id: 1, prerender: true }
+      get :show, id: 1, prerender: true
       assert_includes(response.body, 'Hello')
     end
   end
@@ -30,9 +30,9 @@ class PagesControllerTest < ActionController::TestCase
   WebpackerHelpers.when_webpacker_available do
     test 'it mounts components from the dev server' do
       WebpackerHelpers.with_dev_server do
-        get :show, params: { id: 1, prerender: true }
+        get :show, id: 1, prerender: true
         assert_match /Hello<!--.*--> from Webpacker/, response.body
-        get :show, params: { id: 1, prerender: true, greeting: 'Howdy' }
+        get :show, id: 1, prerender: true, greeting: 'Howdy'
         assert_match /Howdy<!--.*--> from Webpacker/, response.body
       end
     end
