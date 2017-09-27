dccc8ded0908881a3dc0c60e45d57b573e045d9e
Greg <neonmd@hotmail.co.uk>
2017-09-27 18:27:02 +0100

    Correct all deprecation warnings in addons
    Reference: https://facebook.github.io/react/blog/2017/04/07/react-v15.5.0.html
    
    Replace -> With
    react-addons-transition-group -> react-transition-group/TransitionGroup
    react-addons-css-transition-group -> react-transition-group/CSSTransitionGroup
    react-addons-update -> immutability-helper
    react-addons-test-utils -> react-dom/test-utils
    
    Removed
    react-addons-linked-state-mixin
      use Value and onChange
    react-addons-create-fragment
      React16 has fragments
    react-addons-pure-render-mixin
      use React.PureComponent
    react-addons-perf
      see https://facebook.github.io/react/docs/optimizing-performance.html#profiling-components-with-the-chrome-performance-tab

--- react-builds/addons-object.js
+++ react-builds/addons-object.js
@@ -1,14 +1,10 @@
 var addons = {};
-addons.TransitionGroup = require("react-addons-transition-group");
-addons.CSSTransitionGroup = require("react-addons-css-transition-group");
-addons.LinkedStateMixin = require("react-addons-linked-state-mixin");
-addons.createFragment = require("react-addons-create-fragment");
-addons.update = require("react-addons-update");
-addons.PureRenderMixin = require("react-addons-pure-render-mixin");
+addons.TransitionGroup = require("react-transition-group/TransitionGroup");
+addons.CSSTransitionGroup = require("react-transition-group/CSSTransitionGroup");
+addons.update = require("immutability-helper");
 
 if (process.env.NODE_ENV !== "production") {
-  addons.TestUtils = require("react-addons-test-utils");
-  addons.Perf = require("react-addons-perf");
+  addons.TestUtils = require("react-dom/test-utils");
 }
 
 module.exports = addons;
