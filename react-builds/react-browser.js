e59469477473a9d4e73cb873a093163a2fd0a6f2
Greg Myers <neonmd@hotmail.co.uk>
2017-09-30 22:35:53 +0100

    Add PropTypes to default react bundle

--- react-builds/react-browser.js
+++ react-builds/react-browser.js
@@ -1,5 +1,7 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
+var PropTypes = require("prop-types");
 
 window.React = React;
 window.ReactDOM = ReactDOM;
+window.PropTypes = PropTypes;
--- react-builds/react-server.js
+++ react-builds/react-server.js
@@ -1,5 +1,7 @@
 var React = require("react");
 var ReactDOMServer = require("react-dom/server");
+var PropTypes = require("prop-types");
 
 global.React = React;
 global.ReactDOMServer = ReactDOMServer;
+global.PropTypes = PropTypes;
