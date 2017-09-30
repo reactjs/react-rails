7e450abdaa9d630588aa8e9f737102f45660e7bc
Greg Myers <neonmd@hotmail.co.uk>
2017-09-30 22:09:23 +0100

    Add reactCreateBuild to addons

--- react-builds/addons-object.js
+++ react-builds/addons-object.js
@@ -2,6 +2,7 @@ var addons = {};
 addons.TransitionGroup = require("react-transition-group/TransitionGroup");
 addons.CSSTransitionGroup = require("react-transition-group/CSSTransitionGroup");
 addons.update = require("immutability-helper");
+addons.createReactClass = require("create-react-class");
 
 if (process.env.NODE_ENV !== "production") {
   addons.TestUtils = require("react-dom/test-utils");
