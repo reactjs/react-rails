38cf9ec9d89940a249b680b77ad98ef947df463d
Rahul Chanila <chanila.rahul@gmail.com>
2017-12-28 19:45:13 +1100

    :fire: use React.Fragment instead of div

--- lib/generators/templates/component.js.jsx
+++ lib/generators/templates/component.js.jsx
@@ -8,17 +8,13 @@
 <% end -%>
 
   render: function() {
-<% if attributes.size > 0 -%>
     return (
-      <div>
+      <React.Fragment>
 <% attributes.each do |attribute| -%>
-        <div><%= attribute[:name].titleize %>: {this.props.<%= attribute[:name].camelize(:lower) %>}</div>
+        <%= attribute[:name].titleize %>: {this.props.<%= attribute[:name].camelize(:lower) %>}
 <% end -%>
-      </div>
+      </React.Fragment>
     );
-<% else -%>
-    return <div />;
-<% end -%>
   }
 });
 <%= file_footer %>
