07ff6adc9b1ae4b40bc8712bb9ee355b8d548117
Greg Myers <neonmd@hotmail.co.uk>
2018-02-05 19:24:44 +0000

    fix(turbolinks): do not trigger DOMContentLoaded in Turbolinks5

--- react_ujs/src/events/turbolinks.js
+++ react_ujs/src/events/turbolinks.js
@@ -1,13 +1,11 @@
 module.exports = {
   // Turbolinks 5+ got rid of named events (?!)
   setup: function(ujs) {
-    ujs.handleEvent('DOMContentLoaded', ujs.handleMount)
     ujs.handleEvent('turbolinks:render', ujs.handleMount)
     ujs.handleEvent('turbolinks:before-render', ujs.handleUnmount)
   },
 
   teardown: function(ujs) {
-    ujs.removeEvent('DOMContentLoaded', ujs.handleMount)
     ujs.removeEvent('turbolinks:render', ujs.handleMount)
     ujs.removeEvent('turbolinks:before-render', ujs.handleUnmount)
   },
