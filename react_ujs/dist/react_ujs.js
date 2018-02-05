63adfbe88dd13783ad4df873526c2ffd72da5b78
Greg Myers <neonmd@hotmail.co.uk>
2018-02-05 20:00:05 +0000

    fix(turbolinks) Switch to Load instead of Render for triggering

--- react_ujs/dist/react_ujs.js
+++ react_ujs/dist/react_ujs.js
@@ -433,12 +433,12 @@ module.exports = {
 module.exports = {
   // Turbolinks 5+ got rid of named events (?!)
   setup: function(ujs) {
-    ujs.handleEvent('turbolinks:render', ujs.handleMount)
+    ujs.handleEvent('turbolinks:load', ujs.handleMount)
     ujs.handleEvent('turbolinks:before-render', ujs.handleUnmount)
   },
 
   teardown: function(ujs) {
-    ujs.removeEvent('turbolinks:render', ujs.handleMount)
+    ujs.removeEvent('turbolinks:load', ujs.handleMount)
     ujs.removeEvent('turbolinks:before-render', ujs.handleUnmount)
   },
 }
