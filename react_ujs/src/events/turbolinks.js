94f58e27a3858430b1a084cb99d2c69076bf7a05
Jerry Bailey <jkbailey@gmail.com>
2019-10-19 13:46:46 -0700

    Call handleUnmount before rendering a new page (#1020)

--- react_ujs/src/events/turbolinks.js
+++ react_ujs/src/events/turbolinks.js
@@ -2,11 +2,11 @@ module.exports = {
   // Turbolinks 5+ got rid of named events (?!)
   setup: function(ujs) {
   	ujs.handleEvent('turbolinks:load', ujs.handleMount);
-    ujs.handleEvent('turbolinks:before-render', ujs.handleMount);
+    ujs.handleEvent('turbolinks:before-render', ujs.handleUnmount);
   },
 
   teardown: function(ujs) {
   	ujs.removeEvent('turbolinks:load', ujs.handleMount);
-    ujs.removeEvent('turbolinks:before-render', ujs.handleMount);
+    ujs.removeEvent('turbolinks:before-render', ujs.handleUnmount);
   },
 }
