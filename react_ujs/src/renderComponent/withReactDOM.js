acdee60bd13d0e71e81ad6a2c03879ab84493088
Edmond Chui <edmond.c@getjobber.com>
2020-04-25 17:05:47 -0600

    mirror getConstructor structure

---
+++ react_ujs/src/renderComponent/withReactDOM.js
@@ -0,0 +1,8 @@
+// Render React component via ReactDOM, for example:
+//
+// - `renderComponent("hydrate", component, node, props)` -> `ReactDOM.hydrate(component, node);`
+// - `renderComponent("render", component, node, props)` -> `ReactDOM.render(component, node);`
+//
+module.exports = function(renderFunctionName, component, node) {
+  ReactDOM[renderFunctionName](component, node);
+};
