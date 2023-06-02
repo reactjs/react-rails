37e6f022ad44be64800a759c8597a082c8fd35a6
Mostafa Ahangarha <ahangarha@riseup.net>
2023-06-02 18:09:42 +0330

    Improve file name and required packages

--- test/dummy/config/webpack/production.js
+++ test/dummy/config/webpack/production.js
@@ -1,6 +1,6 @@
 // Below code should get refactored but the current way that rails/webpacker v5
 // does the globals, it's tricky
-const webpackConfig = require('./ServerClientOrBoth')
+const webpackConfig = require('./serverClientOrBoth')
 
 const productionEnvOnly = (_clientWebpackConfig, _serverWebpackConfig) => {
   // place any code here that is for production only
