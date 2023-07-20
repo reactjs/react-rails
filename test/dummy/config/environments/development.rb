711791225dbc89a108ee160c6cfc43e498cdb743
Judah Meek <judah.meek@gmail.com>
2023-07-20 16:07:22 -0500

    remove action_mailer references

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -15,9 +15,6 @@ Dummy::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 end
--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -33,11 +33,6 @@ Dummy::Application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
