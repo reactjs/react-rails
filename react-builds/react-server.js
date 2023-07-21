5a6238d2ba49472e284d4f2adffa7313517b021f
Judah Meek <judah.meek@gmail.com>
2023-07-20 23:31:19 -0500

    add TextEncoder library

--- react-builds/react-server.js
+++ react-builds/react-server.js
@@ -2,8 +2,11 @@ var React = require("react");
 var ReactDOMServer = require("react-dom/server");
 var createReactClass = require("create-react-class");
 var PropTypes = require("prop-types");
+var {TextEncoder, TextDecoder} = require("fastestsmallesttextencoderdecoder");
 
 global.React = React;
 global.ReactDOMServer = ReactDOMServer;
 global.createReactClass = createReactClass;
 global.PropTypes = PropTypes;
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
