09e9dc5177594820a7b4324ca773044d15580c6f
Justin Gordon <justin@shakacode.com>
2023-07-31 00:30:27 -0700

    Release 3.0.0-rc.0

--- lib/react/rails/version.rb
+++ lib/react/rails/version.rb
@@ -4,6 +4,6 @@ module React
   module Rails
     # If you change this, make sure to update VERSIONS.md
     # and republish the UJS by updating package.json and `bundle exec rake ujs:publish`
-    VERSION = "2.7.1"
+    VERSION = "3.0.0.rc.0"
   end
 end
