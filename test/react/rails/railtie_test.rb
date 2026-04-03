1450fa877b3ed62f1e5e04babbf471447282a3da
Justin Gordon <justin@shakacode.com>
2026-04-02 23:05:18 -1000

    Fix Propshaft asset version cache busting

--- test/react/rails/railtie_test.rb
+++ test/react/rails/railtie_test.rb
@@ -3,11 +3,32 @@
 require "test_helper"
 
 class RailtieTest < ActionDispatch::IntegrationTest
+  VersionedAssets = Struct.new(:version)
+  PropshaftLikeAssembly = Struct.new(:config)
+
   test "reloaders are configured after initializers are loaded" do
     @test_file = File.expand_path("../../dummy/app/pants/yfronts.js", File.dirname(__FILE__))
     FileUtils.touch @test_file
     results = Dummy::Application.reloaders.map(&:updated?)
 
     assert_includes(results, true)
+  end
+
+  test "cache busting updates asset environments with a direct version" do
+    assets = VersionedAssets.new("1.0")
+
+    React::Rails::Railtie.append_react_build_to_assets_version!(assets, "development")
+
+    assert_equal "1.0-react-development", assets.version
+  end
+
+  test "cache busting updates asset assembly configs with a version" do
+    config = ActiveSupport::OrderedOptions.new
+    config.version = "1.0"
+    assembly = PropshaftLikeAssembly.new(config)
+
+    React::Rails::Railtie.append_react_build_to_assets_version!(assembly, "production")
+
+    assert_equal "1.0-react-production", config.version
   end
 end
